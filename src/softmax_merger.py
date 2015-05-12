ee3f5f0e037a78f3c6eb8cc02edea24df90db2e2
Vladimir Iglovikov <iglovikov@gmail.com>
2015-05-11 20:24:29 -0700

    softmax merger added

--- src/softmax_merger.py
+++ src/softmax_merger.py
@@ -3,7 +3,7 @@ __author__ = 'Vladimir Iglovikov'
 '''
 This scripts merges several csv files that are used to perform Kaggle submission
 
-It performs average regression
+It performs softmax regression
 Right now it works for Wallmart competition
 '''
 
@@ -11,13 +11,23 @@ import os
 import sys
 import pandas as pd
 import time
+import math
+import numpy as np
+import decimal
+
+def logit(p):
+  return -math.log(p/(1-p))
+
+def inv_logit(p):
+  return 1 / (1 + decimal.Decimal(p).exp())
+
 
 files = sys.argv[1:]
+
 try:
-  files.remove('mean_merger.py')
+  files.remove('softmax_merger.py')
 except:
   pass
-
 
 data = [pd.read_csv(fName) for fName in files]
 ids = data[0]['id']
@@ -27,10 +37,13 @@ submission = pd.DataFrame()
 
 ind = 0
 for df in data:
-  result[ind] = df['units']
+  result[ind] = df['units'].apply(inv_logit)
   ind += 1
 
 submission['units'] = result.mean(axis=1)
+
+submission['units'] = submission['units'].apply(logit)
+
 submission['id'] = ids
 
 submission.to_csv('{timestamp}.csv'.format(timestamp=time.time()), index=False)
