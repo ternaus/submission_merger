2bf61e6b7ff138e1243ff9acf3b8b97dd2034339
Vladimir Iglovikov <iglovikov@gmail.com>
2015-07-24 16:45:49 -0700

    Geometric mean for avito added.

---
+++ src/geometric_mean_merger_avito.py
@@ -0,0 +1,45 @@
+from __future__ import division
+__author__ = 'Vladimir Iglovikov'
+'''
+This scripts merges several csv files that are used to perform Kaggle submission
+
+It performs geometric average regression
+Right now it works for avito competition
+'''
+
+import os
+import sys
+import pandas as pd
+import time
+import math
+
+files = sys.argv[1:]
+try:
+  files.remove('geometric_mean_merger_avito.py')
+except:
+  pass
+
+
+data = [pd.read_csv(fName) for fName in files]
+ids = data[0]['ID']
+
+result = pd.DataFrame()
+submission = pd.DataFrame()
+
+ind = 0
+
+submission['ID'] = ids
+
+submission['IsClick'] = 1
+
+for df in data:
+  submission['IsClick'] = submission['IsClick'] * df['IsClick']
+  
+power = 1.0 / len(data)
+
+submission['IsClick'] = submission['IsClick'].apply(lambda x: round(math.pow(x, power))).astype(int)
+
+
+submission['ID'] = ids
+
+submission.to_csv('{timestamp}.csv'.format(timestamp=time.time()), index=False)
