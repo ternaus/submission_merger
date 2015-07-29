59d09956ff8bbd1ca73d2835ed10d3065a9d34bc
Vladimir Iglovikov <iglovikov@gmail.com>
2015-07-29 14:58:24 -0700

    script to perfrom  mean average for liberty competition added.

---
+++ src/mean_merger_liberty.py
@@ -0,0 +1,35 @@
+from __future__ import division
+__author__ = 'Vladimir Iglovikov'
+'''
+This scripts merges several csv files that are used to perform Kaggle submission
+
+It performs average
+'''
+
+import os
+import sys
+import pandas as pd
+import time
+
+files = sys.argv[1:]
+try:
+  files.remove('mean_merger_liberty.py')
+except:
+  pass
+
+
+data = [pd.read_csv(fName) for fName in files]
+ids = data[0]['Id']
+
+result = pd.DataFrame()
+submission = pd.DataFrame()
+
+ind = 0
+for df in data:
+  result[ind] = df['Hazard']
+  ind += 1
+
+submission['Hazard'] = result.mean(axis=1)
+submission['Id'] = ids
+
+submission.to_csv('{timestamp}.csv'.format(timestamp=time.time()), index=False)
