e1cd2927e9073076ff0789a0e9e8e4876ef07f0f
Vladimir Iglovikov <iglovikov@gmail.com>
2015-08-17 11:08:30 -0700

    merger for ICDM added

--- src/ICDM_merger.py
+++ src/ICDM_merger.py
@@ -26,19 +26,21 @@ ids = data[0]['device_id']
 result = pd.DataFrame()
 submission = pd.DataFrame()
 
-ind = 0
+submission['device_id'] = ids
+submission['cookie_id'] = ''
+
+
 for df in data:
-  result[ind] = df['cookie_id'].apply(lambda x: x + ' ')
-  ind += 1
+  submission['cookie_id'] = submission['cookie_id'] + df['cookie_id'].apply(lambda x: x + ' ')
 
 def helper(x):
   result = list(set(x.strip().split()))
   result = ' '.join(result)
   return result
 
-submission['cookie_id'] = result.sum()
+# submission['cookie_id'] = result.sum()
 submission['cookie_id'] = submission['cookie_id'].apply(helper, 1)
 
-submission['device_id'] = ids
+# submission['device_id'] = ids
 
 submission.to_csv('ICDM_{timestamp}.csv'.format(timestamp=time.time()), index=False)
